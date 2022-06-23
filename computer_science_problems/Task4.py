6e8bd2f31aeee891664b99d85e60b647dfaecf5b
nikolaos karapas <nikos.karapas86@gmail.com>
2022-06-23 17:17:30 +0300

    in progress l think i finished

--- computer_science_problems/Task4.py
+++ computer_science_problems/Task4.py
@@ -25,3 +25,19 @@ Print a message:
 The list of numbers should be print out one per line in lexicographic order with no duplicates.
 """
 
+#time complexity is O(n+n+n+n+n+1+1+nlogn) ----> O(n )   space compexity is O(n)
+outgoingCalls =[call[0] for call in calls] #O(n)
+inGoingCalls =[call[1] for call in calls] #O(n)
+sendingtexts =[text[0] for text in texts] #O(n)
+receivingtexts =[text[1] for text in texts] #O(n)
+telemarketers =[] #O(1)
+
+for phone in outgoingCalls:#O(n)
+    if phone not in inGoingCalls or phone not in sendingtexts or phone not in receivingtexts:
+        telemarketers.append(phone)#O(1)
+        
+telemarketers=sorted(telemarketers)#O(nlogn)
+for telemarket  in telemarketers:
+    print(telemarket)
+
+
