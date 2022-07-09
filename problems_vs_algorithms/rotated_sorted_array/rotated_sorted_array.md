7fe2b5f17253369af37e547757b4db8fa9fa0cf7
nikolaos karapas <nikos.karapas86@gmail.com>
2022-07-09 16:36:56 +0300

    problems one and two

---
+++ problems_vs_algorithms/rotated_sorted_array/rotated_sorted_array.md
@@ -0,0 +1,21 @@
+## You are given a sorted array which is rotated at some random pivot point.
+
+## Example: [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]
+
+## You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+## You can assume there are no duplicates in the array and your algorithm's runtime complexity must be in the order of O(log n).
+
+## Example:
+
+## Input: nums = [4,5,6,7,0,1,2], target = 0, Output: 4
+
+## well the requirement algorithm's runtime complexity must be in the order of O(log n).
+
+## we slould use binary search
+
+## to solve that problem we find the the lowerest value in the rotated array thats the pivot
+
+## then we create two subArrays
+
+## then we use binarySearch to find the target number in each subArray . time complexity for the solution is `O(log n)`
---
+++ problems_vs_algorithms/rotated_sorted_array/rotated_sorted_array.py
@@ -0,0 +1,79 @@
+def rotated_array_search(input_list, number):
+    """
+    Find the index by searching in a rotated sorted array
+
+    Args:
+       input_list(array), number(int): Input array to search and the target
+    Returns:
+       int: Index or -1
+    """
+    if len(input_list) == 0:
+        return -1
+    if len(input_list) == 1:
+        if input_list[0] == number:
+            return 0
+        else:
+            return -1
+
+    start = 0
+    end = len(input_list) - 1
+    finding = 0
+    if input_list[start] < input_list[end]:
+        finding = binary_search(start, end, input_list, number)
+    else:
+        pivot = find_pivot(start, end, input_list)
+        if number >= input_list[pivot] and number <= input_list[end]:
+            finding = binary_search(pivot, end, input_list, number)
+        else:
+            finding = binary_search(start, pivot-1, input_list, number)
+    return finding
+
+
+def find_pivot(start, end, inputList):
+    mid = start + (end - start) // 2
+    pivot = 0
+    if inputList[mid] > inputList[mid + 1]:
+        return mid + 1
+    else:
+        if inputList[start] > inputList[mid]:
+            pivot = find_pivot(start, mid-1, inputList)
+        else:
+            pivot = find_pivot(mid+1, end, inputList)
+    return pivot
+
+
+def binary_search(start, end, inputList, target):
+
+    while start <= end:
+        middle = (start+end)//2
+        if inputList[middle] == target:
+            return middle
+        else:
+            if inputList[middle] < target:
+                start = middle + 1
+            else:
+                end = middle - 1
+    return -1
+
+
+def linear_search(input_list, number):
+    for index, element in enumerate(input_list):
+        if element == number:
+            return index
+    return -1
+
+
+def test_function(test_case):
+    input_list = test_case[0]
+    number = test_case[1]
+    if linear_search(input_list, number) == rotated_array_search(input_list, number):
+        print("Pass")
+    else:
+        print("Fail")
+
+
+test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 6])
+test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 1])
+test_function([[6, 7, 8, 1, 2, 3, 4], 8])
+test_function([[6, 7, 8, 1, 2, 3, 4], 1])
+test_function([[6, 7, 8, 1, 2, 3, 4], 10])
